51eee9fd03df8e5ea0d2dfe5458b92eef9ff8b96
Andre Smith <andrepsmithjr@gmail.com>
2014-01-15 14:19:22 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CodeFellowsChapterThree::Application.config.secret_key_base = 'b817dd854b7822a52e83cc2b8c8aa64f61a2b48235fa5b9ac9b2bba60ea122636579729860d29126949c24dcc236bba2522733ce84674ea2e93a8db4673cee1c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CodeFellowsChapterThree::Application.config.session_store :cookie_store, key: '_Code-Fellows-Chapter-Three_session'
